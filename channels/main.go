b7ca74f73220b1b91bff4750255c2e908daeb29d
hrantm <hrant.mirijanyan@gmail.com>
2024-07-05 00:01:09 -0700

    Add channels

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func() {
+			time.Sleep(5 * time.Second)
+			checkLink(l, c)
+		}()
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		log.Panic(err)
+		c <- link
+		return
+	}
+	fmt.Println(link, "is Up")
+	c <- link
 }
